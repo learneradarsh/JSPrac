f4e5d182f9bed9c5cc80ee6e7c51eca8254d356c
learneradarsh <learner.adarsh@gmail.com>
2021-03-15 04:47:21 +0530

    modal show and hide on click

--- modal/script.js
+++ modal/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overLay = document.querySelector('.overlay');
+const showModalBtns = document.querySelectorAll('.show-modal');
+
+function showModal() {
+  modal.classList.remove('hidden');
+  overLay.classList.remove('hidden');
+}
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overLay.classList.add('hidden');
+}
+
+showModalBtns.forEach(showModalBtn =>
+  showModalBtn.addEventListener('click', showModal)
+);
+
+document.querySelector('.close-modal').addEventListener('click', closeModal);
