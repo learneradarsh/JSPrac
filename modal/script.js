435d7f5f62e63292607dde8ce979f71d3874c7c3
learneradarsh <learner.adarsh@gmail.com>
2021-03-15 05:04:03 +0530

    added event listener for Esc key

--- modal/script.js
+++ modal/script.js
@@ -4,6 +4,19 @@ const modal = document.querySelector('.modal');
 const overLay = document.querySelector('.overlay');
 const showModalBtns = document.querySelectorAll('.show-modal');
 
+showModalBtns.forEach(showModalBtn =>
+  showModalBtn.addEventListener('click', showModal)
+);
+
+document.querySelector('.close-modal').addEventListener('click', closeModal);
+document.querySelector('.overlay').addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
 function showModal() {
   modal.classList.remove('hidden');
   overLay.classList.remove('hidden');
@@ -13,9 +26,3 @@ function closeModal() {
   modal.classList.add('hidden');
   overLay.classList.add('hidden');
 }
-
-showModalBtns.forEach(showModalBtn =>
-  showModalBtn.addEventListener('click', showModal)
-);
-
-document.querySelector('.close-modal').addEventListener('click', closeModal);
