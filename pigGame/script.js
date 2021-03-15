8149c25d65afa9101cfcb8ac9e22c33cb3e3bd3f
learneradarsh <learner.adarsh@gmail.com>
2021-03-15 05:49:33 +0530

    roll dice done without condition

--- pigGame/script.js
+++ pigGame/script.js
@@ -1,16 +1,85 @@
-"use strict";
+'use strict';
+
+const playerOneScorePlaceholder = document.querySelector('#score--0');
+const playerTwoScorePlaceholder = document.querySelector('#score--1');
+const playerOneCurrentScorePlaceholder = document.querySelector('#current--0');
+const playerTwoCurrentScorePlaceholder = document.querySelector('#current--1');
+const playerOneSection = document.querySelector('.player--0');
+const playerTwoSection = document.querySelector('.player--1');
+
+const playerOneScore = {
+  finalScore: 0,
+  currentScore: 0,
+};
+
+const playerTwoScore = {
+  finalScore: 0,
+  currentScore: 0,
+};
+
+startNewGame();
+
+function updatePlayerOneScore() {
+  playerOneScore.finalScore += playerOneScore.currentScore;
+  playerOneScore.currentScore = 0;
+  playerOneScorePlaceholder.textContent = playerOneScore.finalScore;
+  playerOneCurrentScorePlaceholder.textContent = playerOneScore.currentScore;
+}
+
+function updatePlayerTwoScore() {
+  playerTwoScore.finalScore += playerTwoScore.currentScore;
+  playerTwoScore.currentScore = 0;
+  playerTwoScorePlaceholder.textContent = playerTwoScore.finalScore;
+  playerTwoCurrentScorePlaceholder.textContent = playerTwoScore.currentScore;
+}
+
+function togglePlayer() {
+  if (playerOneSection.classList.contains('player--active')) {
+    playerTwoSection.classList.add('player--active');
+    playerOneSection.classList.remove('player--active');
+  } else {
+    playerOneSection.classList.add('player--active');
+    playerTwoSection.classList.remove('player--active');
+  }
+}
+
+function startNewGame() {
+  rollTheDice();
+  playerOneCurrentScorePlaceholder.textContent = '0';
+  playerTwoCurrentScorePlaceholder.textContent = '0';
+  playerOneScorePlaceholder.textContent = '0';
+  playerTwoScorePlaceholder.textContent = '0';
+}
 
 function rollTheDice() {
   const imageSrcList = [
-    "dice-5.png",
-    "dice-1.png",
-    "dice-2.png",
-    "dice-3.png",
-    "dice-4.png",
-    "dice-6.png",
+    'dice-5.png',
+    'dice-1.png',
+    'dice-2.png',
+    'dice-3.png',
+    'dice-4.png',
+    'dice-6.png',
   ];
   const randomIndex = Math.trunc(Math.random() * 6);
-  document.querySelector(".dice").src = `${imageSrcList[randomIndex]}`;
+  document.querySelector('.dice').src = `${imageSrcList[randomIndex]}`;
+  if (playerOneSection.classList.contains('player--active')) {
+    playerOneScore.currentScore++;
+    playerOneCurrentScorePlaceholder.textContent = playerOneScore.currentScore;
+  } else {
+    playerTwoScore.currentScore++;
+    playerTwoCurrentScorePlaceholder.textContent = playerTwoScore.currentScore;
+  }
 }
 
-document.querySelector(".btn--roll").addEventListener("click", rollTheDice);
+function hold() {
+  if (playerOneSection.classList.contains('player--active')) {
+    updatePlayerOneScore();
+  } else {
+    updatePlayerTwoScore();
+  }
+  togglePlayer();
+}
+
+document.querySelector('.btn--roll').addEventListener('click', rollTheDice);
+document.querySelector('.btn--new').addEventListener('click', startNewGame);
+document.querySelector('.btn--hold').addEventListener('click', hold);
